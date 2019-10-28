c65a84ec4dd0a2fe78d8c9c5186dfe48d2f9df78
Gregmus <gregmus4@gmail.com>
2019-10-28 17:46:17 +0200

    move RelativePosition to this pkg

--- types.go
+++ types.go
@@ -1,5 +1,7 @@
 package pkg
 
+import "math"
+
 type Algorithm interface {
 	Start(anthill Pos, birth Pos)
 	Do(fields [9]FieldType, round int) (field uint8, action Action)
@@ -13,4 +15,17 @@ func (p *Pos) X() uint {
 
 func (p *Pos) Y() uint {
 	return p[1]
+}
+
+/* add field with format
+	0 1 2
+	3 4 5
+	6 7 8
+to input position
+*/
+func (p Pos) RelativePosition(field uint8) Pos {
+	return Pos{
+		p.X() + uint(math.Mod(float64(field+3), 3)-1),
+		p.Y() + uint(math.Floor(float64(field/3))-1), //nolint
+	}
 }
