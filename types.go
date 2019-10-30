f867971d0ea98f6c90a2567e4f9df9f4db1c4034
Gregmus <gregmus4@gmail.com>
2019-10-30 15:45:28 +0200

    replace RelativePosition on Add method

--- types.go
+++ types.go
@@ -1,7 +1,5 @@
 package pkg
 
-import "math"
-
 type Algorithm interface {
 	Start(anthill Pos, birth Pos)
 	Do(fields [5][5]FieldType, round int) (target Pos, action Action)
@@ -17,15 +15,7 @@ func (p *Pos) Y() int {
 	return p[1]
 }
 
-/* add field with format
-	0 1 2
-	3 4 5
-	6 7 8
-to input position
-*/
-func (p Pos) RelativePosition(field uint8) Pos {
-	return Pos{
-		p.X() + int(math.Mod(float64(field+3), 3)-1),
-		p.Y() + int(math.Floor(float64(field/3))-1), //nolint
-	}
+func (p Pos) Add(pos Pos) {
+	p[0] += pos.X()
+	p[1] += pos.Y()
 }
