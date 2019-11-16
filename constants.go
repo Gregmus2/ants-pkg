8ec6197bddf5106fcae0c82421fbae6f3dcb3e28
gregmus <gregmus4@gmail.com>
2019-11-16 13:09:07 +0200

    add iota

--- constants.go
+++ constants.go
@@ -2,19 +2,23 @@ package pkg
 
 type FieldType uint8
 
-const NoField FieldType = 0
-const EmptyField FieldType = 1
-const FoodField FieldType = 2
-const AllyField FieldType = 3
-const EnemyField FieldType = 4
-const WallField FieldType = 5
-const AntField FieldType = 6
-const AnthillField FieldType = 7
+const (
+	NoField = iota
+	EmptyField
+	FoodField
+	AllyField
+	EnemyField
+	WallField
+	AntField
+	AnthillField
+)
 
 type Action uint8
 
-const NoAction Action = 0
-const AttackAction Action = 1
-const EatAction Action = 2
-const MoveAction Action = 3
-const DieAction Action = 4
+const (
+	NoAction = iota
+	AttackAction
+	EatAction
+	MoveAction
+	DieAction
+)
