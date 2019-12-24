45c6e0605ac799d959ea855fb9451fe341bbdac7
Kostiantyn Osichenko <kostiantyn.osichenko@libify.com>
2019-12-24 11:31:03 +0200

    change algorithm interface

--- algorithm.go
+++ algorithm.go
@@ -1,9 +1,10 @@
 package pkg
 
 type Algorithm interface {
+	Start(anthillID int, birthPos *Pos)
 	Do(antID int, fields [5][5]FieldType, round int, posDiff *Pos) (target *Pos, action Action)
 	OnAntDie(antID int)
 	OnAnthillDie(anthillID int)
 	OnAntBirth(antID int, anthillID int)
-	OnNewAnthill(invaderID int, birthPos *Pos) // antID; position relative anthill
+	OnNewAnthill(invaderID int, birthPos *Pos, anthillID int) // antID; position relative anthill
 }
